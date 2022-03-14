648c174f9ab70e06fcc84f40ae0af6f6032f9e8e
omar-diane <omar.diane@laplateforme.io>
2022-03-14 16:54:39 +0100

    Job05 fait.

---
+++ jour01/job05/script.js
@@ -0,0 +1,9 @@
+var jourssemaines = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+
+function afficherjourssemaines(){
+    for(var i=0; i < jourssemaines.length; i++){
+        console.log(jourssemaines[i]);
+    } 
+}
+
+afficherjourssemaines();
