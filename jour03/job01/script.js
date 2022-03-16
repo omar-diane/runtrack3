61e4096e97cd5d17698bceb148d3c3af19578f4d
omar-diane <omar.diane@laplateforme.io>
2022-03-16 18:13:48 +0100

    Job01 fait.

---
+++ jour03/job01/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function()
+{   
+    console.log($('p'))
+    console.log($('#button'))
+    $('p').hide()
+
+   
+    $('#button').click(function()
+    {
+        
+        $('p').show()
+    })
+
+    $('#hide').click(function()
+    {
+        
+        $('html').hide()
+    })
+})
