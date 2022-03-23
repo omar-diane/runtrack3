675f4caba3d1689b72eebd15d76c33b1eae34198
omar-diane <omar.diane@laplateforme.io>
2022-03-23 19:52:12 +0100

    Jour 6 job03 à vérifier si tout est bon et job04 en cours !

---
+++ jour06/job04/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <li><a href="#">Accueil</a></li>
+        <li><a href="#">Présentation</a></li>
+        <li><a href="#">Contact</a></li>
+    </header>
+    
+</body>
+</html>
